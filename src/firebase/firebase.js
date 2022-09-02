3f1a6889107458c4678963f2d5a74c63085f199a
luuu-xu <a550763321@gmail.com>
2022-09-02 15:28:27 -0400

    Firebase Hosting, Firebase set up.

---
+++ src/firebase/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA1jcHmo0psYB8CrNbE1LvQEYkSSqabJPI",
+  authDomain: "toock-32270.firebaseapp.com",
+  projectId: "toock-32270",
+  storageBucket: "toock-32270.appspot.com",
+  messagingSenderId: "774385905782",
+  appId: "1:774385905782:web:61a4949402285268126f57"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
