b83421136c8521645b61225ba1456865a4ecc74c
luuu-xu <a550763321@gmail.com>
2022-09-08 14:57:37 -0400

    Firebase Auth implemented into LoginPage. Creating accounts are now possible, and loging in as well.

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -1,22 +1,33 @@
 import '../styles/LoginPage.css';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { getCurrentUserFromAuth, auth, registerWithEmailPassword, signOutUser, userSigninWithEmailPassword, updateUserNameAndPhoto } from '../firebase/firebaseAuth';
+import { useEffect, useState } from 'react';
+import { onAuthStateChanged } from 'firebase/auth';
 
 function LoginPage({ signingUp }) {
-  // const [signingUp, setSigningUp] = useState(signUpPage);
+  const [user, setUser] = useState(null);
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      setUser(user);
+    })
+  }, []);
 
-  // function onFormChange() {
-  //   setSigningUp(!signingUp);
-  // }
-
-  return (
-    <div className="login-page">
-      <LoginPageHeader signingUp={signingUp} />
-      <PartnerLoginList signingUp={signingUp} />
-      <BreakLine />
-      <LoginForm signingUp={signingUp} />
-      <FormChange signingUp={signingUp} />
-    </div>
-  );
+  if (!user) {
+    return (
+      <div className="login-page">
+        <LoginPageHeader signingUp={signingUp} />
+        <PartnerLoginList signingUp={signingUp} />
+        <BreakLine />
+        {signingUp ? <SignupForm /> : <LoginForm />}
+        <FormChange signingUp={signingUp} />
+        <button onClick={() => console.log(getCurrentUserFromAuth())}>Check user is signed in?</button>
+        <button onClick={signOutUser}>log out</button>
+        <p>{user ? `user.email${user.email}` : "no user"}</p>
+      </div>
+    );
+  } else {
+    return <RedirectToHome />
+  }
 }
 
 function LoginPageHeader({ signingUp }) {
@@ -54,26 +65,87 @@ function BreakLine() {
   );
 }
 
-function LoginForm({ signingUp }) {
+function LoginForm() {
+  const [userNotFound, setUserNotFound] = useState(false);
+  const [wrongPassword, setWrongPassword] = useState(false);
+
+  async function onSubmitLogin(event) {
+    event.preventDefault();
+    const email = event.target.email.value;
+    const password = event.target.password.value;
+    const errorCode = await userSigninWithEmailPassword(email, password);
+    // console.log(errorCode === "auth/wrong-password");
+    if (errorCode === "auth/wrong-password") {
+      setUserNotFound(false);
+      setWrongPassword(true);
+    } else if (errorCode === "auth/user-not-found") {
+      setUserNotFound(true);
+      setWrongPassword(false);
+    } else {
+      setUserNotFound(false);
+      setWrongPassword(false);
+    }
+  }
+
   return (
-      <form className="login-form">
-        <label>Email address
-          <input type="email" name="email" required />
-        </label>
-        <label>Password
-          <input type="password" name="password" required />
-        </label>
-        {signingUp ? 
-          <label>Username
-            <input type="text" name="username" required />
-          </label>
-          :
-          <a>Forgot password?</a>
-        }
-        <button type="submit">
-          {signingUp ? "Sign up" : "Log in"}
-        </button>
-      </form>
+    <form className="login-form" onSubmit={onSubmitLogin}>
+      <label>Email address
+        <input type="email" name="email" required />
+      </label>
+      <label>Password
+        <input type="password" name="password" required />
+      </label>
+      <a>Forgot password?</a>
+      {userNotFound ? <p className="wrong-p">Email is not registered yet. Create an account now.</p> : null}
+      {wrongPassword ? <p className="wrong-p">Wrong email or password. Please try again.</p> : null}
+      <button type="submit">
+        Log in
+      </button>
+    </form>
+  );
+}
+
+function SignupForm() {
+  const [emailUsed, setEmailUsed] = useState(false);
+  const [invalidEmail, setInvalidEmail] = useState(false);
+
+  async function onSubmitSignup(event) {
+    event.preventDefault();
+    const email = event.target.email.value;
+    const password = event.target.password.value;
+    const registerErrorCode = await registerWithEmailPassword(email, password);
+    if (registerErrorCode === "auth/email-already-in-use") {
+      setEmailUsed(true);
+      setInvalidEmail(false);
+    } else if (registerErrorCode === "auth/invalid-email") {
+      setEmailUsed(false);
+      setInvalidEmail(true);
+    } else {
+      setEmailUsed(false);
+      setInvalidEmail(false);
+    }
+    const username = event.target.username.value;
+    const updateErrorCode = await updateUserNameAndPhoto(username);
+    console.log(updateErrorCode);
+  }
+
+  return (
+    <form className="login-form" onSubmit={onSubmitSignup}>
+      <label>Email address
+        <input type="email" name="email" required />
+      </label>
+      <label>Password
+        <input type="password" name="password" minLength="6" required />
+      </label>
+      <label>Username
+        <input type="text" name="username" required />
+      </label>
+      {emailUsed ? <p className="wrong-p">Email is already registered. Try logging in.</p> : null}
+      {invalidEmail ? <p className="wrong-p">Invalid Email address. Try again.</p> : null}
+      <button type="submit">
+        Sign up
+      </button>
+    </form>
   );
 }
 
@@ -86,6 +158,25 @@ function FormChange({ signingUp }) {
       </Link>
     </p>
   );
+}
+
+function RedirectToHome() {
+  let navigate = useNavigate();
+
+  // useEffect(() => {
+  //   setTimeout(() => {
+  //     navigate("/");
+  //   }, 2000);
+  // }, []);
+
+  return (
+    <div className="redirect-to-home">
+      <p>You are signed in.</p>
+      <p>Redirecting to home page now...</p>
+      <Link to="/">Click here if not redirected</Link>
+      <button onClick={signOutUser}>log out</button>
+    </div>
+  )
 }
 
 export default LoginPage;
--- src/firebase/firebaseAuth.js
+++ src/firebase/firebaseAuth.js
@@ -1,6 +1,75 @@
 import app from "./firebase";
-import { getAuth } from "firebase/auth";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged, signOut, updateProfile } from "firebase/auth";
 
-// Initialize Firebase Authentication and get a reference to the service
+// Initialize Firebase Authentication and get a reference to the service.
 const auth = getAuth(app);
 
+// Create user with email/password.
+async function registerWithEmailPassword(email, password) {
+  try {
+    const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+    const user = userCredential.user;
+    console.log(user);
+  } catch(error) {
+    console.error(error);
+    // Error codes: "auth/email-already-in-use", "auth/invalid-email".
+    return error.code;
+  }
+}
+
+// Sign in user with email/password.
+async function userSigninWithEmailPassword(email, password) {
+  try {
+    const userCredential = await signInWithEmailAndPassword(auth, email, password);
+    const user = userCredential.user;
+    console.log(user);
+  } catch(error) {
+    console.error(error);
+    // Error codes: "auth/wrong-password", "auth/user-not-found".
+    return error.code;
+  }
+}
+
+// Listen to auth state changes.
+// function monitorAuthState() {
+//   onAuthStateChanged(auth, (user) => {
+//     if (user) {
+//       // User is signed in.
+//       console.log("monitorAuth" + user.uid);
+//       return user;
+//     } else {
+//       // User is not signed in or signed out.
+//     }
+//   });
+// }
+
+// Returns current user, or null if not signed in.
+function getCurrentUserFromAuth() {
+  return auth.currentUser;
+}
+
+// Signs out the current user.
+function signOutUser() {
+  signOut(auth);
+}
+
+// Updates user's displayName and photoURL.
+async function updateUserNameAndPhoto(userName, photoUrl = null) {
+  try {
+    updateProfile(auth.currentUser, {
+      displayName: userName, photoURL: photoUrl
+    });
+  } catch(error) {
+    console.log(error);
+    return error.code;
+  }
+}
+ 
+export { 
+  registerWithEmailPassword, 
+  userSigninWithEmailPassword, 
+  auth,
+  getCurrentUserFromAuth,
+  signOutUser,
+  updateUserNameAndPhoto
+};
